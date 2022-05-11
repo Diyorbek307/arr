a818f1f4b8253b11de442a3cb3bd3256accef9ef
Diyorbek307 <104882866+Diyorbek307@users.noreply.github.com>
2022-05-11 13:28:14 +0500

    dsasda
    sadsad

---
+++ java.js
@@ -0,0 +1,38 @@
+
+// let one = 0;
+// let num = prompt('Здравствуйте это фибаначи, ввдетите любое число?')
+// let two = 1;
+
+
+// for (let i = 0; i < num; i++) {
+//   let ans = one + two;
+
+//   one = two;
+//   two = ans;
+
+//   console.log(ans);
+// }
+
+
+
+// let name = prompt(`What is you name?`)
+// let surname = prompt(`What is you surname?`)
+// let age = +prompt(`How old are you?`)
+
+// let full = [
+//    ` ${name}${surname}${age}@gmail.com`,
+//     `${name}${age}${surname}@gmail.com`,
+//    ` ${surname}${name}${age}@gmail.com`,
+//    ` ${surname}${age}${name}@gmail.com`,
+//     `${age}${name}${surname}@gmail.com`,
+//    ` ${age}${surname}${name}@gmail.com`
+// ]
+
+
+// let email = () =>{
+//     let obj = Math.floor(Math.random() * 3)
+//     alert(full[obj])
+// }
+
+// email()
+
